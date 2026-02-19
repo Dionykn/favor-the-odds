1da2a5c417c291f2afc8ead172ad48d0afca1a69
Dionykn <dionklein12@hotmail.com>
2026-02-19 21:11:57 +0100

    Restructured 2

--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -1,5 +1,7 @@
 extends Node
+class_name Deck
 
+var cards = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func build():
+	pass
+	
+func shuffle():
+	cards.shuffle()
+
+func draw() -> String:
+	return cards.pop_back()
--- Scripts/player_ui.gd
+++ Scripts/player_ui.gd
@@ -1,10 +1,19 @@
 extends Control
+class_name PlayerUI
 
+# Functions updating UI
+func show_hand(cards:Array, face_up:bool):
+	pass
+
+func show_stock(card:String, face_up:bool):
+	pass
+
+func show_discards(dscards:Array):
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
